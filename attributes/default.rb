fef60c407c81ecd45dd98a8d147860c608f5b253
Andrew Williams <awilliams@intoxitrack.net>
2013-09-26 15:16:21 -0500

    Add basic installation functionality

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['zfs']['rpm_filename'] = 'zfs-release-1-2.el6.noarch.rpm'
+default['zfs']['rpm_checksum'] = '96592a2cc74af'
+default['zfs']['rpm_url'] = "http://archive.zfsonlinux.org/epel/#{node['zfs']['rpm_filename']}"
