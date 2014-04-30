cad1ac750f120cf598a32e4df86e66ee13076154
Andrew Williams <awilliams@intoxitrack.net>
2014-04-30 14:48:06 -0500

    Add missing build-essential depends

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,6 @@ description      "Installs/Configures ZFS on Linux systems"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+depends 'build-essential'
+
 %w{centos redhat}.each { |os| supports os }
